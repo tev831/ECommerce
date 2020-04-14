9cb8d704b965ca8a6f9c4ecdb76a8e5065c2311d
tevin0831 <tevin0831@gmail.com>
2020-04-14 13:10:33 -0700

    Add project files.

---
+++ ECommerce.ProductCatalog/ProductCatalog.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace ECommerce.ProductCatalog
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class ProductCatalog : StatefulService // Stateful because it holds onto the products information as a DB. 
+    {
+        private IProductRepository _repo;
+
+        public ProductCatalog(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new ServiceReplicaListener[0];
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            _repo = new ServiceFabricProductRepository(this.StateManager);
+
+            var product1 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Cool Mouse 1",
+                Description = "This is a cool mouse",
+                Price = 60,
+                Availability = 30
+            };
+
+            var product2 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Bad Mouse 1",
+                Description = "This is a bad mouse",
+                Price = 20,
+                Availability = 100
+            };
+
+            await _repo.AddProduct(product1);
+            await _repo.AddProduct(product2);
+
+            IEnumerable<Product> all = await _repo.GetAllProducts();
+
+
+            // TODO: Replace the following sample code with your own logic 
+            //       or remove this RunAsync override if it's not needed in your service.
+            //var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+
+            //while (true)
+            //{
+            //    cancellationToken.ThrowIfCancellationRequested();
+
+            //    using (var tx = this.StateManager.CreateTransaction())
+            //    {
+            //        var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+
+            //        ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
+            //            result.HasValue ? result.Value.ToString() : "Value does not exist.");
+
+            //        await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
+
+            //        // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+            //        // discarded, and nothing is saved to the secondary replicas.
+            //        await tx.CommitAsync();
+            //    }
+
+            //    await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            //}
+        }
+    }
+}
---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,65 @@
+﻿using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECommerce.ProductCatalog
+{
+    class ServiceFabricProductRepository : IProductRepository
+    {
+        private readonly IReliableStateManager _stateManager;
+
+        public ServiceFabricProductRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            // Returns a dictionary. Gets it in the back or creates one if it doesn't exist.
+            IReliableDictionary<Guid, Product> products = await _stateManager
+                .GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+
+            // Everything is done via transactions
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                await products.AddOrUpdateAsync(tx, product.Id, product, (id, value) => product);
+
+                await tx.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            IReliableDictionary<Guid, Product> products = await _stateManager
+                .GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+            var results = new List<Product>();
+
+            // Everything is done via transactions
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                // Get all products from Service Fabric async. (Incurs a call cost)
+                // Returns enumerable instead of list because there can be millions of products. 
+                Microsoft.ServiceFabric.Data.IAsyncEnumerable<KeyValuePair<Guid, Product>> allProducts =
+                    await products.CreateEnumerableAsync(tx, EnumerationMode.Unordered);
+
+                // Get product enumerator from Service Fabric async. (Incurs a call cost)
+                using (Microsoft.ServiceFabric.Data.IAsyncEnumerator<KeyValuePair<Guid, Product>> enumerator =
+                    allProducts.GetAsyncEnumerator())
+                {
+                    while (await enumerator.MoveNextAsync(CancellationToken.None))
+                    {
+                        KeyValuePair<Guid, Product> current = enumerator.Current;
+                        results.Add(current.Value);
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
